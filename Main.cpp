d1a37d2baaba70f6eb2176d14b82848423bd55fd
Xndr <moermanxander@gmail.com>
2024-06-04 15:32:36 +0200

    Color / error compiling

--- Main.cpp
+++ Main.cpp
@@ -12,12 +12,13 @@
 // Vertices coordinates
 GLfloat vertices[] =
 {
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+	// -------------- Coordinates	---------------- /	------- Color --------- //
+	-0.50f, -0.50f * float(sqrt(3)) / 3,	 0.00f,		0.80f, 0.30f, 0.02f,	// Lower left corner
+	 0.50f, -0.50f * float(sqrt(3)) / 3,	 0.00f,		0.80f, 0.30f, 0.02f,	// Lower right corner
+	 0.00f,  0.50f * float(sqrt(3)) * 2 / 3, 0.00f,		1.00f, 0.60f, 0.32f,	// Upper corner
+	-0.25f,  0.50f * float(sqrt(3)) / 6,	 0.00f,		0.90f, 0.45f, 0.17f,	// Inner left
+	 0.25f,  0.50f * float(sqrt(3)) / 6,	 0.00f,		0.90f, 0.45f, 0.17f,	// Inner right
+	 0.00f, -0.50f * float(sqrt(3)) / 3,	 0.00f,		0.80f, 0.30f, 0.02f		// Inner down
 };
 
 // Indices for vertices order
@@ -25,10 +26,8 @@ GLuint indices[] =
 {
 	0, 3, 5, // Lower left triangle
 	3, 2, 4, // Lower right triangle
-	5, 4, 1 // Upper triangle
+	5, 4, 1  // Upper triangle
 };
-
-
 
 int main()
 {
@@ -78,12 +77,14 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO to VAO
-	VAO1.LinkVBO(VBO1, 0);
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
+	GLuint uniId = glGetUniformLocation(shaderProgram.ID, "scale");
 
 
 	// Main while loop
@@ -95,6 +96,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
+		glUniform1f(uniId, 0.5f);
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 		// Draw primitives, number of indices, datatype of indices, index of indices
