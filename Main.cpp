de94717c0b71efc33652297f1568862c2ac67186
Xndr <moermanxander@gmail.com>
2024-06-04 16:39:22 +0200

    Textures

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<stb/stb_image.h>
 
 #include"shaderClass.h"
 #include"VAO.h"
@@ -10,7 +11,7 @@
 
 
 // Vertices coordinates
-GLfloat vertices[] =
+GLfloat vertices3Triangles[] =
 {
 	// -------------- Coordinates	---------------- /	------- Color --------- //
 	-0.50f, -0.50f * float(sqrt(3)) / 3,	 0.00f,		0.00f, 1.00f, 0.00f,	// Lower left corner
@@ -22,11 +23,28 @@ GLfloat vertices[] =
 };
 
 // Indices for vertices order
-GLuint indices[] =
+GLuint indices3Triangles[] =
 {
 	0, 3, 5, // Lower left triangle
 	3, 2, 4, // Lower right triangle
 	5, 4, 1  // Upper triangle
+};
+
+// Vertices coordinates
+GLfloat verticesSquare[] =
+{
+	// ---- Coordinates	---- /	----- Color ----- / Texture coords //
+	-0.5f, -0.5f,	0.0f,		1.0f, 0.0f, 0.0f,	0.0f, 0.0f,
+	-0.5f,	0.5f,	0.0f,		0.0f, 1.0f, 0.0f,	0.0f, 1.0f,
+	 0.5f,  0.5f,	0.0f,		0.0f, 0.0f, 1.0f,	1.0f, 1.0f,
+	 0.5f, -0.5f,	0.0f,		1.0f, 1.0f, 1.0f,	1.0f, 0.0f
+};
+
+// Indices for vertices order
+GLuint indicesSquare[] =
+{
+	0, 2, 1,	// upper triangle
+	0, 3, 2		// lower triangle
 };
 
 int main()
@@ -72,13 +90,14 @@ int main()
 	VAO1.Bind();
 
 	// Generates Vertex Buffer Object and links it to vertices
-	VBO VBO1(vertices, sizeof(vertices));
+	VBO VBO1(verticesSquare, sizeof(verticesSquare));
 	// Generates Element Buffer Object and links it to indices
-	EBO EBO1(indices, sizeof(indices));
+	EBO EBO1(indicesSquare, sizeof(indicesSquare));
 
 	// Links VBO to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
@@ -86,6 +105,31 @@ int main()
 
 	GLuint uniId = glGetUniformLocation(shaderProgram.ID, "scale");
 
+	// Texture
+	int widthImg, heightImg, numColCh;
+	unsigned char* bytes = stbi_load("Shrok.jpg", &widthImg, &heightImg, &numColCh, 0);
+
+	// create texture
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// keep all pixels
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	// wrap
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	shaderProgram.Activate();
+	glUniform1i(tex0Uni, 0);
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
@@ -97,10 +141,11 @@ int main()
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
 		glUniform1f(uniId, 0.5f);
+		glBindTexture(GL_TEXTURE_2D, texture);
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
@@ -113,6 +158,7 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	glDeleteTextures(1, &texture);
 	shaderProgram.Delete();
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
